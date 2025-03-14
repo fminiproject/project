1871d137c3178833fafcc2597c4ba39272272b42
Kwangryong-Choi <ckrjjang2@naver.com>
2025-03-14 20:01:53 +0900

    250314

--- minproject/src/main/java/hp_setting/hp_joinok.java
+++ minproject/src/main/java/hp_setting/hp_joinok.java
@@ -1,4 +1,4 @@
-/*
+
 package hp_setting;
 
 import java.io.IOException;
@@ -22,12 +22,37 @@ public class hp_joinok extends HttpServlet {
 		String hp_title = request.getParameter("hp_title");
 		String ad_mail = request.getParameter("ad_mail");
 		String use_point = request.getParameter("use_point");
-		String join_point = request.getParameter("join_point");
-		String join_lv = request.getParameter("join_lv");
+		String join_point = String.valueOf(request.getParameter("join_point"));
+		String join_lv = String.valueOf(request.getParameter("join_lv"));
+		String corp_name = request.getParameter("corp_name");
+		String busi_num = request.getParameter("busi_num");
+		String ceo_name = request.getParameter("ceo_name");
+		String ceo_num = request.getParameter("ceo_num");
+		String busi_report_num = request.getParameter("busi_report_num");
+		String busi_num_more = request.getParameter("busi_num_more");
+		String corp_addnum = request.getParameter("corp_addnum");
+		String corp_add = request.getParameter("corp_add");
+		String info_ad_name = request.getParameter("info_ad_name");
+		String info_ad_mail = request.getParameter("info_ad_mail");
+		String no_bank = request.getParameter("no_bank");
+		String account_num = request.getParameter("account_num");
+		String card_pay = request.getParameter("card_pay");
+		String phone_pay = request.getParameter("phone_pay");
+		String coupon_pay = request.getParameter("coupon_pay");
+		String pay_po_min = request.getParameter("pay_po_min");
+		String pay_po_max = request.getParameter("pay_po_max");
+		String receipt = request.getParameter("receipt");
+		String deli_corp = request.getParameter("deli_corp");
+		String deli_pay = request.getParameter("deli_pay");
+		String deli_day = request.getParameter("deli_day");
 		
-		int result = this.hi.hp_in(hp_title,ad_mail,use_point,join_point,join_lv);
+		int result = this.hi.hp_in(hp_title,ad_mail,use_point,join_point,join_lv,
+				corp_name,busi_num,ceo_name,ceo_num,busi_report_num,busi_num_more,
+				corp_addnum,corp_add,info_ad_name,info_ad_mail,no_bank,account_num,
+				card_pay,phone_pay,coupon_pay,pay_po_min,pay_po_max,receipt,
+				deli_corp,deli_pay,deli_day);
 		this.pw = response.getWriter();
-		if(result > 0) {
+		if(result != 0) {
 			this.pw.write("<script>"
 					+ "alert('정상적으로 설정이 저장되었습니다.');"
 					+ "location.href='./hp_join_list.do';"
@@ -41,4 +66,4 @@ public class hp_joinok extends HttpServlet {
 	}
 
 }
-*/
+
--- minproject/src/main/java/hp_setting/hp_select.java
+++ minproject/src/main/java/hp_setting/hp_select.java
@@ -14,21 +14,25 @@ public class hp_select {
 	m_db db = new m_db();
 	ResultSet rs = null;
 	
-	ArrayList<String> data = null;
-	ArrayList<ArrayList<String>> alldata = null;
+	ArrayList<Object> data = null;
+	ArrayList<ArrayList<Object>> alldata = null;
 	
-	public ArrayList<ArrayList<String>> hp_datalist_bs(){
+	public ArrayList<ArrayList<Object>> hp_datalist(){
 		
 		try {
 			this.con = this.db.getConnection();
-			this.sql = "select * from ";
+			this.sql = "select * from homepage;";
 			this.ps = this.con.prepareStatement(this.sql);
 			this.rs = this.ps.executeQuery();
 			
-			this.data = new ArrayList<String>();
+			this.data = new ArrayList<Object>();
 			
 			while(this.rs.next()) {
-				this.data.add(this.rs.getString("bidx"));
+				this.data.add(this.rs.getString("hp_title"));
+				this.data.add(this.rs.getString("ad_mail"));
+				this.data.add(this.rs.getString("use_point"));
+				this.data.add(this.rs.getInt("join_point"));
+				this.data.add(this.rs.getInt("join_lv"));
 				this.data.add(this.rs.getString("corp_name"));
 				this.data.add(this.rs.getString("busi_num"));
 				this.data.add(this.rs.getString("ceo_name"));
@@ -39,6 +43,17 @@ public class hp_select {
 				this.data.add(this.rs.getString("corp_add"));
 				this.data.add(this.rs.getString("info_ad_name"));
 				this.data.add(this.rs.getString("info_ad_mail"));
+				this.data.add(this.rs.getString("no_bank"));
+				this.data.add(this.rs.getInt("account_num"));
+				this.data.add(this.rs.getString("card_pay"));
+				this.data.add(this.rs.getString("phone_pay"));
+				this.data.add(this.rs.getString("coupon_pay"));
+				this.data.add(this.rs.getInt("pay_po_min"));
+				this.data.add(this.rs.getInt("pay_po_max"));
+				this.data.add(this.rs.getString("receipt"));
+				this.data.add(this.rs.getString("deli_corp"));
+				this.data.add(this.rs.getInt("deli_pay"));
+				this.data.add(this.rs.getString("deli_day"));
 				
 				this.alldata.add(this.data);
 			}
